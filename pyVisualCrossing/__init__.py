da021065dec5cc7d25953ee360f2037ca06b8212
Bjarne Riis <bjarne@briis.com>
2023-09-23 13:33:44 +0000

    Bump to 0.1.7

--- pyVisualCrossing/__init__.py
+++ pyVisualCrossing/__init__.py
@@ -5,6 +5,6 @@ from pyVisualCrossing.api import VisualCrossing
 from pyVisualCrossing.data import ForecastData, ForecastDailyData, ForecastHourlyData
 
 __title__ = "pyVisualCrossing"
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 __author__ = "briis"
 __license__ = "MIT"
